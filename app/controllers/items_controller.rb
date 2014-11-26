fb8326bf0c13a1f832facab840ca6c2835efea55
dianaberenguer <dianaberenguer@gmail.com>
2014-11-26 16:18:56 +0000

    images and home

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,28 @@ class ItemsController < ApplicationController
 		@user = User.find(params[:user_id])
 	end
 
+	def create
+		@user = User.find(params[:user_id])
+		@item = Item.new(entry_params)
+
+		if @user.save
+			flash[:notice] = "User has been created succesfully"
+			redirect_to root_path
+
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	private
+
+	def entry_params
+		params.require(:item).permit(:name, :picture, :price, :date, :description)
+	end
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ActiveRecord::Base
 	has_many :participations
 	has_many :users, through: :participations
 
+	has_attached_file :picture, :styles => { :large => "300x500>", :medium => "200x300>", :thumb => "100x200>" }
+
 end
