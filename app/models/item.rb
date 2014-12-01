f97e47ad4ffa2bdeff5f7a9c95ac545a3cdb9591
dianaberenguer <dianaberenguer@gmail.com>
2014-12-01 11:11:43 +0000

    index item & links

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
 	has_many :participations
 	has_many :users, through: :participations
 
-	has_attached_file :picture, :styles => { :large => "300x500>", :medium => "200x300>", :thumb => "100x200>" }
+	has_attached_file :picture, :styles => { :large => "300x500>", :medium => "290x390>", :thumb => "100x200>" }
 
 	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 	validates_presence_of :name
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Item.create(name: "BBQ", picture: nil, price: "120", description: "BBQ new", date: "2014-12-17 00:00:00", picture_file_name: "30016610_i2.jpg", picture_content_type: "image/jpeg", picture_file_size: 65979, picture_updated_at: "2014-12-01 10:53:12")
+
+User.create(name: "Diana", avatar_file_name: "Diana.jpg", avatar_content_type: "image/jpeg", avatar_file_size: 1346825, avatar_updated_at: "2014-12-01 10:52:04", email: "dianaberenguer@gmail.com", location: "Barcelona", description: "Hola hola hola")
